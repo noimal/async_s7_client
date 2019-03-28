88f9330799a3cd7104de0b545c3daa4f19cbf3d8
ermanimer <ermanimer@gmail.com>
2019-03-28 11:39:10 +0300

    v1.0.0

---
+++ async_s7_client/S7ClientException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace async_s7_client {
+    public class S7ClientException : Exception {
+        public int Code { get; }
+
+        public S7ClientException(int code, string message) : base(message) {
+            Code = code;
+        }
+
+        public override string ToString() {
+            return $"{Code.ToString()} : {Message}";
+        }
+    }
+}
